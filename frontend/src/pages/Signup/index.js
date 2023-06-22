7c08f12b22bd0b8912e0acdd532eab83fe7daa5e
rafaelbok <108283975+rafaelbok@users.noreply.github.com>
2023-06-22 11:59:08 -0300

    update

--- frontend/src/pages/Signup/index.js
+++ frontend/src/pages/Signup/index.js
@@ -36,7 +36,7 @@ const Copyright = () => {
 		<Typography variant="body2" color="textSecondary" align="center">
 			© {new Date().getFullYear()}
 			{" - "}
-			<Link color="inherit" href={system.url || "https://github.com/rtenorioh/Press-Ticket"}>
+			<Link color="inherit" href={system.url || "https://wasap.com.br"}>
 				{system.name || "Press Ticket"}
 			</Link>
 			{"."}
