0a35b58bf99500bf9e92e3f533c69c623c2ae1fa
rafaelbok <108283975+rafaelbok@users.noreply.github.com>
2023-06-25 13:24:16 -0300

    update

--- backend/src/libs/wbot.ts
+++ backend/src/libs/wbot.ts
@@ -130,28 +130,6 @@ export const initWbot = async (whatsapp: Whatsapp): Promise<Session> => {
         puppeteer: {
           args: [
             "--no-sandbox",
-            "--disable-setuid-sandbox",
-            "--log-level=3",
-            "--no-default-browser-check",
-            "--disable-site-isolation-trials",
-            "--no-experiments",
-            "--ignore-gpu-blacklist",
-            "--ignore-certificate-errors",
-            "--ignore-certificate-errors-spki-list",
-            "--disable-extensions",
-            "--disable-default-apps",
-            "--disable-setuid-sandbox",
-            "--no-sandbox",
-            "--disable-in-process-stack-traces",
-            "--disable-histogram-customizer",
-            "--disable-gl-extensions",
-            "--disable-canvas-aa",
-            "--disable-3d-apis",
-            "--disable-accelerated-2d-canvas",
-            "--disable-accelerated-jpeg-decoding",
-            "--disable-accelerated-mjpeg-decode",
-            "--disable-app-list-dismiss-on-blur",
-            "--disable-accelerated-video-decode"
           ],
           executablePath: process.env.CHROME_BIN || undefined
         }
